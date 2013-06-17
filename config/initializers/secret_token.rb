7108dc727f056f408816286f5119f22890627010
joshua kemp <joshuakemp85@gmail.com>
2013-06-16 21:25:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grandma::Application.config.secret_token = '4da5bcd50707d8c1b8e1cc02af615297a7b20346b0cd2875b15560af84fe00d6dcf3f0a7adde0ce940ba233a96d21e9bc8d2dd932a27a09426e6634c661d1866'
